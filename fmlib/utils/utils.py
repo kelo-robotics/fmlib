f97ba03ab6d869115c70fe8e5bc9ba9487a495b7
Angela Enriquez <angela.enriquez@locomotec.com>
2020-09-23 07:42:10 +0200

    utils: Add dicts to get status names

--- fmlib/utils/utils.py
+++ fmlib/utils/utils.py
@@ -1,6 +1,7 @@
 import yaml
 import json
 from importlib_resources import open_text
+from ropod.structs.status import ActionStatus, TaskStatus
 
 
 def load_file_from_module(module, file_name):
@@ -23,3 +24,7 @@ def load_yaml_config_file(file_name):
     with open(file_name, 'r') as file_handle:
         config = load_yaml(file_handle)
     return config
+
+
+task_status_names = {value: name for name, value in vars(TaskStatus).items() if name.isupper()}
+action_status_names = {value: name for name, value in vars(ActionStatus).items() if name.isupper()}
