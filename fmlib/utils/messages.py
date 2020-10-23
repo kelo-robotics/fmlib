b1d96f8d8fad1ef8adc03fe1e01a9b7c6a951f48
Angela Enriquez <angela.enriquez@locomotec.com>
2020-10-23 12:09:25 +0200

    messages: Iterate through nested lists when formating a msg and a document

--- fmlib/utils/messages.py
+++ fmlib/utils/messages.py
@@ -17,6 +17,11 @@ def format_msg(msg_dict):
                 for prop, value in value.items()}
 
     if isinstance(msg_dict, dict):
+        for prop, value in msg_dict.items():
+            if isinstance(value, list):
+                for i, element in enumerate(value):
+                    if isinstance(element, dict):
+                        value[i] = format_msg(element)
         return _format_msg_keys(msg_dict)
     else:
         return msg_dict
@@ -28,6 +33,11 @@ def format_document(doc_dict):
                 for prop, value in doc.items()}
 
     if isinstance(doc_dict, dict):
+        for prop, value in doc_dict.items():
+            if isinstance(value, list):
+                for i, element in enumerate(value):
+                    if isinstance(element, dict):
+                        value[i] = format_document(element)
         return _format_doc_keys(doc_dict)
     else:
         return doc_dict
