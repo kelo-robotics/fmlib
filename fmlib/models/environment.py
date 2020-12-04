ec84b46a9f3c6881a5319973771490e82d5c020e
Angela Enriquez <angela.enriquez@locomotec.com>
2020-12-04 14:32:15 +0100

    environment: Add method to get intersection point bewteen the lines: line 1:  between point1 and point2 line 2: perpendicular line between self and line 1

--- fmlib/models/environment.py
+++ fmlib/models/environment.py
@@ -52,6 +52,37 @@ class Position(EmbeddedMongoModel):
     def get_distance(self, other):
         return ((self.x - other.x) ** 2 + (self.y - other.y) ** 2) ** 0.5
 
+    def slope(self, other):
+        # m = (y2 - y1) / (x2- x1)
+        return (other.y - self.y)/(other.x - self.x)
+
+    def y_intercept(self, slope):
+        # b = y - mx
+        return self.y - slope * self.x
+
+    def get_intersection_point(self, point1, point2):
+        """Returns the intersection point between the lines:
+            line 1: between point1 and point2
+            line 2: perpendicular line between self and line 1
+        """
+        # Line 1:
+        m1 = point1.slope(point2)
+        b1 = point1.y_intercept(m1)
+
+        # Perpendicular lines have a slope of -1
+        # m1*m2 = -1
+        m2 = -1 /m1
+
+        # Line 2:  (y - self.y) = m2(x - self.x)
+        b2 = self.y_intercept(m2)
+
+        # line1 = line2
+        # m1 * x + b1 = m2 * x + b2
+        x = (b2 - b1) / (m1 - m2)
+        y = m1 * x + b1
+
+        return Position(x, y)
+
     def to_dict(self):
         dict_repr = self.to_son().to_dict()
         dict_repr.pop('_cls')
