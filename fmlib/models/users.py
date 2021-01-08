1112d45c5d8507ebd1b2615fde7183d845658458
Angela Enriquez <angela.enriquez@locomotec.com>
2021-01-08 10:16:20 +0100

    users: Add create_new method

--- fmlib/models/users.py
+++ fmlib/models/users.py
@@ -4,3 +4,9 @@ from pymodm import MongoModel, fields
 class User(MongoModel):
 
     user_id = fields.CharField(primary_key=True)
+
+    @classmethod
+    def create_new(cls, **kwargs):
+        user = cls(**kwargs)
+        user.save()
+        return user
